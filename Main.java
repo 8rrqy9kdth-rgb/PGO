63917a3ef55917b7bc452c1d2c8e4edf2a9d72dd
s35666-eng <s35666@pjwstk.edu.pl>
2026-03-18 09:15:03 +0100

    Zadanie 4: Obsługa wyporzyczeń i scenariusz testowy aplikacji

--- Main.java
+++ Main.java
@@ -1,7 +1,32 @@
 public class Main {
-    public static void
-    main(String[] args) {
-        System.out.println("Hello PGO!");
-    //Zadanie 4 gotowe
+    public static void main(String[] args) {
+        // 1. Tworzymy bibliotekę na 5 książek
+        Biblioteka mojaBiblioteka = new Biblioteka(5);
+
+        // 2. Tworzymy kilka książek
+        Ksiazka k1 = new Ksiazka("Wiedźmin", "Andrzej Sapkowski", 350, true);
+        Ksiazka k2 = new Ksiazka("Harry Potter", "J.K. Rowling", 400, true);
+        Ksiazka k3 = new Ksiazka("Pan Tadeusz", "Adam Mickiewicz", 250, true);
+
+        // 3. Dodajemy książki do biblioteki
+        mojaBiblioteka.dodajKsiazke(k1);
+        mojaBiblioteka.dodajKsiazke(k2);
+        mojaBiblioteka.dodajKsiazke(k3);
+
+        // 4. Tworzymy czytelnika
+        Czytelnik student = new Czytelnik("Ania", "Kowalska", 12345);
+
+        // 5. Symulujemy wypożyczenie
+        System.out.println("--- Stan przed wypożyczeniem ---");
+        student.wypiszDane();
+        mojaBiblioteka.wypiszDostepneKsiazki();
+
+        // Akcja: Ania wypożycza książkę
+        k1.wypozycz();
+        student.zwiekszLiczbaWypozyczen();
+
+        System.out.println("\n--- Stan po wypożyczeniu Wiedźmina ---");
+        student.wypiszDane();
+        k1.wypiszInfo();
     }
 }
